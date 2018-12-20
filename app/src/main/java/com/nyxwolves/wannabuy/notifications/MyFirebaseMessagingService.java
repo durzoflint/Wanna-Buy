2254356fda2ff861d1a3c95376232f8342c4cc5a
durzoflint <abhinavu1201@gmail.com>
2018-12-20 14:24:54 +0530

    Added Notification Code

---
+++ app/src/main/java/com/nyxwolves/wannabuy/notifications/MyFirebaseMessagingService.java
@@ -0,0 +1,15 @@
+package com.nyxwolves.wannabuy.notifications;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d("Abhinav", "Refreshed token: " + token);
+
+        //sendRegistrationToServer(token);
+    }
+}

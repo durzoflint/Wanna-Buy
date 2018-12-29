0455621ac10afd57e0f0ed3b23326bf4d4c03bdc
sibi <sibinehru99@gmail.com>
2018-12-29 23:34:53 +0530

    made corrections -sibi

--- app/src/main/java/com/nyxwolves/wannabuy/activities/RoadWidth.java
+++ app/src/main/java/com/nyxwolves/wannabuy/activities/RoadWidth.java
@@ -5,23 +5,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.nyxwolves.wannabuy.R;
 
 public class RoadWidth extends AppCompatActivity {
 
     Button nextBtn;
+    SeekBar roadWidth;
+    TextView selectedWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_road_width);
 
+        selectedWidth = findViewById(R.id.selected_width);
+
         nextBtn = findViewById(R.id.road_next_btn);
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RoadWidth.this,Budget.class));
+            }
+        });
+
+        roadWidth = findViewById(R.id.road_width_seekbar);
+        roadWidth.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                selectedWidth.setText(Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }

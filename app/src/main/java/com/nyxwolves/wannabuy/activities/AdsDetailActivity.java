fc9857b97bff449730e6ac3c36aec42527d1c8cc
durzoflint <abhinavu1201@gmail.com>
2019-01-27 21:52:08 +0530

    Refactored Activities

--- app/src/main/java/com/nyxwolves/wannabuy/activities/AdsDetailActivity.java
+++ app/src/main/java/com/nyxwolves/wannabuy/activities/AdsDetailActivity.java
@@ -1,15 +1,32 @@
 package com.nyxwolves.wannabuy.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.nyxwolves.wannabuy.R;
 
-public class AdsDetailActivity extends AppCompatActivity {
+public class AdsDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ads_detail);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16f));
     }
 }
